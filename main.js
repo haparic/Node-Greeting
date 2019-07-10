790b8126e798172c331a2af22d2deed30262322c
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-09 18:30:57 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require('./greeting.js');
+const newModule = require('./newModule.js');
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+newModule.goodbyeEnglish();
+
+newModule.goodbyeJapanese();
+
+newModule.goodbyeSpanish();
+
+newModule.goodbyeIcelandic();
