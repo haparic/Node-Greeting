d5d6624e39dca4842c464f4e49738863f930f16a
Hugo Aparicio <hugo.aparicio8@gmail.com>
2019-07-09 18:30:54 -0700

    Create newModule.js

---
+++ newModule.js
@@ -0,0 +1,16 @@
+
+exports.goodbyeEnglish = function(){
+    console.log('Goodbye');
+  }
+  
+  exports.goodbyeJapanese = function(){
+    console.log('Konichiwa');
+  }
+  
+  exports.goodbyeSpanish = function(){
+    console.log('Adiós');
+  }
+  
+  exports.goodbyeIcelandic = function(){
+    console.log('Bless');
+  }
